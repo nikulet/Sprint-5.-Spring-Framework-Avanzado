ae7aa7300acf78347319cf4b41de38a2253fae8d
nikulet <72616263+nikulet@users.noreply.github.com>
2024-07-10 02:35:45 +0200

    Delete Tarea 1 Spring boot API rest/S05T01N02CebanNicusor/S05T01N02CebanNicusor directory

--- Tarea 1 Spring boot API rest/S05T01N02CebanNicusor/S05T01N02CebanNicusor/src/main/java/cat/itacademy/barcelonactiva/ceban/nicusor/s05/t01/n02/exceptions/FlorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.barcelonactiva.ceban.nicusor.s05.t01.n02.exceptions;
-
-public class FlorNotFoundException extends RuntimeException {
-    public FlorNotFoundException(String message) {
-        super(message);
-    }
-}
